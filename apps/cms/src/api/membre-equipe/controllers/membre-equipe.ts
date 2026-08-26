04e22718aea3d11ac2c25192ee4fb2e9e8ecbfaa
Julien-Desbard <julien.desbard@gmail.com>
2026-08-26 11:47:00 +0200

    chore: decommission Strapi CMS (chunk L cutover)
    Payload est validé en prod (contenu, admin, éditions) : suppression
    de apps/cms/, du plan de déploiement Strapi devenu obsolète, et des
    étapes/env vars CI qui lui étaient dédiées.

--- apps/cms/src/api/membre-equipe/controllers/membre-equipe.ts
+++
@@ -1,3 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::membre-equipe.membre-equipe');
--- apps/cms/src/api/membre-equipe/routes/membre-equipe.ts
+++
@@ -1,3 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::membre-equipe.membre-equipe');
